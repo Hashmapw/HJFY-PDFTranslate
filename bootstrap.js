314de99e2abb3ec1ab199b737d768ed1df6ac243
李文韬 <2443912540@qq.com>
2026-08-18 17:51:19 +0800

    feat: 工程骨架 - 插件清单/启动/文档
    - manifest.json: Zotero 7 插件清单 (id: hjfy-pdftranslate@hjfy.top)
    - chrome.manifest: 内容注册 + 右键菜单 overlay
    - bootstrap.js: 启动/加载 core.js 与 plugin.js
    - README.md: 安装与使用说明
    - docs/hjfy_top_使用文档.md: hjfy.top 接口逆向与流程文档

---
+++ bootstrap.js
@@ -0,0 +1,65 @@
+/*
+ * HJFY-PDFTranslate bootstrap (Zotero 7)
+ */
+"use strict";
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+function log(msg) {
+	Zotero.debug("HJFY-PDFTranslate: " + msg);
+}
+
+function startup({ id, version, rootURI }) {
+	log("startup " + version);
+	try {
+		if (!Zotero.HJFY) {
+			// core.js: 纯逻辑 (全局 HJFYCore)
+			Services.scriptloader.loadSubScript(rootURI + "content/scripts/core.js");
+			// plugin.js: Zotero 胶水 (定义 Zotero.HJFYPlugin)
+			Services.scriptloader.loadSubScript(rootURI + "content/scripts/plugin.js");
+			Zotero.HJFY = new Zotero.HJFYPlugin(rootURI);
+			Zotero.HJFY.init();
+		}
+	} catch (e) {
+		log("startup error: " + e + "\n" + (e && e.stack ? e.stack : ""));
+	}
+}
+
+function onMainWindowLoad({ window }) {
+	if (Zotero.HJFY) {
+		try {
+			Zotero.HJFY.onMainWindowLoad(window);
+		} catch (e) {
+			log("onMainWindowLoad error: " + e);
+		}
+	}
+}
+
+function onMainWindowUnload({ window }) {
+	if (Zotero.HJFY) {
+		try {
+			Zotero.HJFY.onMainWindowUnload(window);
+		} catch (e) {
+			log("onMainWindowUnload error: " + e);
+		}
+	}
+}
+
+function shutdown() {
+	if (Zotero.HJFY) {
+		try {
+			Zotero.HJFY.shutdown();
+		} catch (e) {
+			log("shutdown error: " + e);
+		}
+	}
+	Zotero.HJFY = null;
+}
+
+function install() {
+	log("install");
+}
+
+function uninstall() {
+	log("uninstall");
+}
